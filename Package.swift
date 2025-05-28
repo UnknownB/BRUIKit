e10d4830170a58d11b5b4823039f667b5e0580a9
BR <snkp002@gmail.com>
2025-05-28 21:43:53 +0800

    chore: remove platform support in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "BRUIKit",
-    platforms: [
-        .iOS(.v14)
-    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
