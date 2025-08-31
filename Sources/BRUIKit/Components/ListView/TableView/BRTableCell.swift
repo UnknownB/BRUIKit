78307d34115fc55a0728edfae853a46e26380b96
BR <snkp002@gmail.com>
2025-08-31 19:59:14 +0800

    feat: TableView 添加 BRTableCell
    + 遵循 ListView `Base` 架構的系統原始 UI

---
+++ Sources/BRUIKit/Components/ListView/TableView/BRTableCell.swift
@@ -0,0 +1,96 @@
+//
+//  BRTableDefaultCell.swift
+//  BRUIKit
+//
+//  Created by BR on 2025/8/11.
+//
+
+import UIKit
+
+
+public final class BRTableCellModel: Hashable {
+    public var image: UIImage?
+    public var text: String?
+    public var detail: String?
+    public var selectionStyle: UITableViewCell.SelectionStyle = .none
+    public var accessoryType: UITableViewCell.AccessoryType = .none
+    public var accessoryView: UIView? = nil
+    
+    
+    public static func == (lhs: BRTableCellModel, rhs: BRTableCellModel) -> Bool {
+        lhs.text == rhs.text && lhs.detail == rhs.detail
+    }
+
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(text)
+        hasher.combine(detail)
+    }
+    
+    
+    public init(_ text: String?) {
+        self.text = text
+    }
+    
+    
+    @discardableResult
+    public func image(_ image: UIImage?) -> Self {
+        self.image = image
+        return self
+    }
+    
+    @discardableResult
+    public func text(_ text: String?) -> Self {
+        self.text = text
+        return self
+    }
+    
+    @discardableResult
+    public func detail(_ detail: String?) -> Self {
+        self.detail = detail
+        return self
+    }
+    
+    @discardableResult
+    public func selectionStyle(_ style: UITableViewCell.SelectionStyle) -> Self {
+        self.selectionStyle = style
+        return self
+    }
+    
+    @discardableResult
+    public func accessory(_ type: UITableViewCell.AccessoryType, view: UIView?) -> Self {
+        self.accessoryType = type
+        self.accessoryView = view
+        return self
+    }
+    
+}
+
+
+open class BRTableCell: UITableViewCell, BRCellProtocol {
+    public typealias Model = BRTableCellModel
+    
+    
+    public func bind(with model: BRTableCellModel) {
+        if #available(iOS 14.0, *) {
+            // cell 高度適當高度、有間距
+            var config = self.defaultContentConfiguration()
+            config.image = model.image
+            config.text = model.text
+            config.secondaryText = model.detail
+            self.contentConfiguration = config
+            self.selectionStyle = model.selectionStyle
+            self.accessoryType = model.accessoryType
+            self.accessoryView = model.accessoryView
+        } else {
+            // cell 高度切齊、無間距
+            self.imageView?.image = model.image
+            self.textLabel?.text = model.text
+            self.detailTextLabel?.text = model.detail
+            self.selectionStyle = model.selectionStyle
+            self.accessoryType = model.accessoryType
+            self.accessoryView = model.accessoryView
+        }
+    }
+}
+
