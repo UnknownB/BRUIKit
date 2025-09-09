1ae7535df109c395338d2f23a18fd44d2af23c09
BR <snkp002@gmail.com>
2025-09-10 00:56:15 +0800

    feat: ListView 添加 BRSegmentBar
    + 可滑動的標籤頁

---
+++ Sources/BRUIKit/Components/ListView/View/BRSegmentBar.swift
@@ -0,0 +1,107 @@
+//
+//  BRSegmentBar.swift
+//  BRUIKit
+//
+//  Created by BR on 2025/9/9.
+//
+
+import UIKit
+
+
+public final class BRSegmentBar<Button: UIButton>: UIView {
+
+    private let layout = BRLayout()
+    private let selectionGroup = BRSingleSelectGroup<Button>()
+    
+    
+    private let scrollView = UIScrollView()
+        .br.showsHorizontalIndicator(false)
+    
+    
+    private let stackView = UIStackView()
+        .br.axis(.horizontal)
+        .br.spacing(8)
+        .br.alignment(.fill)
+        .br.distribution(.fill)
+    
+    
+    public var buttons: [Button] {
+        selectionGroup.buttons
+    }
+    
+    
+    public var selectedButton: Button? {
+        selectionGroup.selectedButton
+    }
+    
+    
+    public var onSelectionChanged: ((Button?) -> Void)? {
+        get {
+            selectionGroup.didChangeSelection
+        }
+        set {
+            selectionGroup.didChangeSelection = newValue
+        }
+    }
+    
+    
+    // MARK: - LifeCycle
+    
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupLayout()
+    }
+
+    public required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupLayout()
+    }
+
+    
+    // MARK: - UI
+    
+    
+    private func setupLayout() {
+        addSubview(scrollView)
+        scrollView.addSubview(stackView)
+        
+        layout.activate {
+            scrollView.br.left == self.br.left
+            scrollView.br.right == self.br.right
+            scrollView.br.top == self.br.top
+            scrollView.br.bottom == self.br.bottom
+            
+            stackView.br.left == scrollView.contentLayoutGuide.br.left
+            stackView.br.right == scrollView.contentLayoutGuide.br.right
+            stackView.br.top == scrollView.contentLayoutGuide.br.top
+            stackView.br.bottom == scrollView.contentLayoutGuide.br.bottom
+            stackView.br.height == scrollView.frameLayoutGuide.br.height
+        }
+    }
+    
+
+    @MainActor
+    public func addButton(_ button: Button) {
+        stackView.addArrangedSubview(button)
+        selectionGroup.addButton(button)
+    }
+    
+
+    @MainActor
+    public func selectButton(_ button: Button, scrollToVisible: Bool = true) {
+        selectionGroup.selectButton(button)
+        
+        if scrollToVisible {
+            scrollToButton(button)
+        }
+    }
+    
+
+    @MainActor
+    private func scrollToButton(_ button: Button) {
+        scrollView.scrollRectToVisible(button.frame.insetBy(dx: -16, dy: 0), animated: true)
+    }
+    
+    
+}
