91b7a40536ff4afda424605790be5973e1a7e3fc
BR <snkp002@gmail.com>
2025-10-21 22:59:32 +0800

    feat: 新增 UIView+DSL 擴充

--- Sources/BRUIKit/Components/View/UIView+DSL.swift
+++ Sources/BRUIKit/Components/View/UIView+DSL.swift
@@ -119,4 +119,30 @@ public extension BRWrapper where Base: UIView {
     }
     
     
+    @MainActor
+    @discardableResult
+    func addSubView(_ view: UIView) -> Base {
+        base.addSubview(view)
+        return base
+    }
+    
+    
+    /// 添加霧玻璃底圖
+    /// - Parameter style: 霧玻璃樣式
+    @MainActor
+    @discardableResult
+    func insertBlurEffect(_ style: UIBlurEffect.Style, at index: Int = 0) -> Base {
+        let effect = UIBlurEffect(style: style)
+        let effectView = UIVisualEffectView(effect: effect)
+        base.insertSubview(effectView, at: index)
+        BRLayout().activate {
+            effectView.br.top == base.br.top
+            effectView.br.left == base.br.left
+            effectView.br.bottom == base.br.bottom
+            effectView.br.right == base.br.right
+        }
+        return base
+    }
+    
+    
 }
