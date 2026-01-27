5f2925535cf56c788b8c6dbf7b0abbc9092cd6ef
BR <snkp002@gmail.com>
2026-01-27 21:07:32 +0800

    feat: 更新 BRTextFieldGroupView 改繼承 UIStackView

--- Sources/BRUIKit/Components/TextField/BRTextFieldGroupView.swift
+++ Sources/BRUIKit/Components/TextField/BRTextFieldGroupView.swift
@@ -9,8 +9,12 @@ import UIKit
 
 
 /// BRTextFieldGroupView 提供將一組 TextField 整合成單一輸入框的功能，可以用來做常見的驗證碼輸入介面
-open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
+open class BRTextFieldGroupView: UIStackView {
     private let layout = BRLayout()
+    
+    
+    /// 將元件添加自 UIStackView 的 arrangedSubviews 時，若元件屬於 UITextField 類型則會自動更新於此
+    private(set) public var textFields: [UITextField] = []
     
 
     /// 獲得焦點的輸入欄位索引值
@@ -23,26 +27,17 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     
     /// 所有輸入欄位合併字串
     public var combinedText: String {
-        stackView.arrangedSubviews.compactMap {$0 as? TextField }.compactMap(\.text).joined()
+        self.arrangedSubviews.compactMap {$0 as? UITextField }.compactMap(\.text).joined()
     }
-    
-    
-    // MARK: - UI 元件
-
-    
-    private let stackView: UIStackView = UIStackView()
-        .br.axis(.horizontal)
-        .br.alignment(.fill)
-        .br.distribution(.equalSpacing)
     
     
     // MARK: - LifeCycle
     
     
-    public init(with textFields: [TextField]) {
+    public init(with textFields: [UITextField]) {
         super.init(frame: .zero)
         setupLayout()
-        textFields.forEach { addTextField($0) }
+        textFields.forEach { self.addArrangedSubview($0) }
     }
     
 
@@ -52,7 +47,7 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     }
     
     
-    public required init?(coder: NSCoder) {
+    public required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
@@ -66,24 +61,20 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     
     
     private func setupLayout() {
-        self.addSubview(stackView)
-                
-        layout.activate {
-            stackView.br.top == self.br.top
-            stackView.br.bottom == self.br.bottom
-            stackView.br.left == self.br.left
-            stackView.br.right == self.br.right
-        }
+        self.axis = .horizontal
     }
     
     
     // MARK: - Data
     
-
-    @MainActor
-    public func addTextField(_ textField: TextField) {
+    
+    open override func addArrangedSubview(_ view: UIView) {
+        super.addArrangedSubview(view)
+        guard let textField = view as? UITextField else {
+            return
+        }
+        textFields.append(textField)
         adjustText(textField)
-        stackView.addArrangedSubview(textField)
         NotificationCenter.default.addObserver(self, selector: #selector(onTextDidBegin), name: UITextField.textDidBeginEditingNotification, object: textField)
         NotificationCenter.default.addObserver(self, selector: #selector(onTextDidChange), name: UITextField.textDidChangeNotification, object: textField)
         NotificationCenter.default.addObserver(self, selector: #selector(onTextDidEnd), name: UITextField.textDidEndEditingNotification, object: textField)
@@ -91,8 +82,31 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     }
     
     
-    public func removeTextField(_ textField: TextField) {
-        stackView.removeArrangedSubview(textField)
+    open override func insertArrangedSubview(_ view: UIView, at stackIndex: Int) {
+        super.insertArrangedSubview(view, at: stackIndex)
+        guard let textField = view as? UITextField else {
+            return
+        }
+        for index in self.arrangedSubviews.indices where index > stackIndex {
+            if self.arrangedSubviews[index] is UITextField {
+                textFields.insert(textField, at: index)
+                break
+            }
+        }
+        adjustText(textField)
+        NotificationCenter.default.addObserver(self, selector: #selector(onTextDidBegin), name: UITextField.textDidBeginEditingNotification, object: textField)
+        NotificationCenter.default.addObserver(self, selector: #selector(onTextDidChange), name: UITextField.textDidChangeNotification, object: textField)
+        NotificationCenter.default.addObserver(self, selector: #selector(onTextDidEnd), name: UITextField.textDidEndEditingNotification, object: textField)
+        updateCompletedFlag()
+    }
+    
+    
+    open override func removeArrangedSubview(_ view: UIView) {
+        super.removeArrangedSubview(view)
+        guard let textField = view as? UITextField else {
+            return
+        }
+        textFields.br.removingFirstOccurrence(of: textField)
         textField.removeFromSuperview()
         NotificationCenter.default.removeObserver(self, name: UITextField.textDidBeginEditingNotification, object: textField)
         NotificationCenter.default.removeObserver(self, name: UITextField.textDidChangeNotification, object: textField)
@@ -116,19 +130,17 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
         guard let index = focusedTextFieldIndex(from: sender) else {
             return
         }
-        guard let textField = stackView.arrangedSubviews[index] as? TextField else {
-            return
-        }
+        let textField = textFields[index]
         adjustText(textField)
         updateCompletedFlag()
                         
         if textField.text?.count == 0 {
             if index > 0 {
-                stackView.arrangedSubviews[index - 1].becomeFirstResponder()
+                textFields[index - 1].becomeFirstResponder()
             }
         } else {
-            if index < stackView.arrangedSubviews.count - 1 {
-                stackView.arrangedSubviews[index + 1].becomeFirstResponder()
+            if index < textFields.count - 1 {
+                textFields[index + 1].becomeFirstResponder()
             } else {
                 textField.resignFirstResponder()
             }
@@ -145,15 +157,15 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     
     
     private func focusedTextFieldIndex(from sender: Notification) -> Int? {
-        guard let textField = sender.object as? TextField else {
+        guard let textField = sender.object as? UITextField else {
             return nil
         }
-        let index = stackView.arrangedSubviews.firstIndex(of: textField)
+        let index = textFields.firstIndex(of: textField)
         return index
     }
     
     
-    private func adjustText(_ textField: TextField) {
+    private func adjustText(_ textField: UITextField) {
         if let text = textField.text {
             textField.text = String(text.suffix(1))
         }
@@ -161,9 +173,7 @@ open class BRTextFieldGroupView<TextField: BRTextField>: UIView {
     
     
     private func updateCompletedFlag() {
-        isComplete = stackView.arrangedSubviews
-            .compactMap { $0 as? TextField }
-            .allSatisfy { ($0.text?.count ?? 0) == 1 }
+        isComplete = textFields.allSatisfy { ($0.text?.count ?? 0) == 1 }
     }
 
     
