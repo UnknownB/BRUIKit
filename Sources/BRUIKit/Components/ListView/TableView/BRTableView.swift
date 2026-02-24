a223d9bbd7c74d7c2826bbba129088751ee0b95e
BR <snkp002@gmail.com>
2026-02-24 20:42:29 +0800

    feat: BRTableView 添加初始化方式

--- Sources/BRUIKit/Components/ListView/TableView/BRTableView.swift
+++ Sources/BRUIKit/Components/ListView/TableView/BRTableView.swift
@@ -32,6 +32,14 @@ public class BRTableView: UIView {
     }
     
     
+    public init(style: UITableView.Style) {
+        self.tableView = UITableView(frame: .zero, style: style)
+        self.adapter = BRTableAdapter(tableView: tableView)
+        super.init(frame: .zero)
+        setupLayout()
+    }
+    
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
